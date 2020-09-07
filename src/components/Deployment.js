fd80c1a7d1c1c28cc2fdfd7871f88d2ce7607d85
Hoang Manh Duc <gelous3@gmail.com>
2020-09-07 16:52:18 +0700

    create, update from uploaded file (fix)

--- src/components/Deployment.js
+++ src/components/Deployment.js
@@ -14,9 +14,13 @@ class Deployment extends React.Component {
     this.fileReader = new FileReader();
     this.fileReader.onload = event =>{
       this.setState({fileContent:JSON.parse(this.fileReader.result)},()=>{
-        console.log(this.state.fileContent);
+        //console.log(JSON.stringify(this.state.fileContent));
+        this.createDeploymentFromFile();
       });
     }
+  }
+  componentDidMount(){
+    this.getDeploymentList();
   }
   onNameChange =event=>{
     this.setState({ name: event.target.value });
@@ -51,6 +55,8 @@ class Deployment extends React.Component {
   onFormSubmit = event => {
     event.preventDefault();
   };
+
+//Delete choosen deployment  
   deleteDeployment = ()=>{
     if (this.state.select===''){
       alert('Please choose a deployment..');
@@ -65,10 +71,12 @@ class Deployment extends React.Component {
       .then(response => response.text())
       .then(result => {
         this.setState({log:result});
-        alert(JSON.parse(result).status);
+        alert("Delete "+JSON.parse(result).status);
       })
       .catch(error => this.setState({log:error}));
   }
+
+//Create a deployment from fields (test purpose)
   createDeployment =()=>{
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -118,11 +126,54 @@ class Deployment extends React.Component {
     fetch("http://127.0.0.1:8000/apis/apps/v1/namespaces/default/deployments/", requestOptions)
       .then(response => response.text())
       .then(result => {
-        alert(JSON.parse(result).kind === "Deployment" ? "Success" : "Failure");
+        alert(JSON.parse(result).kind === "Deployment" ? "Successfully Created" : "Failure");
         this.setState({log:result})
       })
       .catch(error => this.setState({log:error}));
   }
+
+//Create a deployment from uploaded file
+  createDeploymentFromFile =()=>{
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    let requestOptions = {
+      method: 'POST',
+      body: JSON.stringify(this.state.fileContent),
+      redirect: 'follow',
+      headers:myHeaders
+    };
+
+    fetch("http://127.0.0.1:8000/apis/apps/v1/namespaces/default/deployments/", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        if (JSON.parse(result).status === "Failure" && JSON.parse(result).reason === "AlreadyExists"){
+          this.updateDeploymentFromFile()
+        }else alert(JSON.parse(result).kind === "Deployment" ? "Successfully Created" : "Failure");        
+        this.setState({log:result})
+      })
+      .catch(error => this.setState({log:error}));
+  }
+
+//update Deployment from uploaded file
+  updateDeploymentFromFile =()=>{
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    let requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: JSON.stringify(this.state.fileContent),
+      redirect: 'follow'
+    };
+    fetch(`http://127.0.0.1:8000/apis/apps/v1/namespaces/default/deployments/${this.state.fileContent.metadata.name}`, requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        alert(JSON.parse(result).kind === "Deployment" ? "Successfully Updated" : "Failure");
+        this.setState({log:result});
+      })
+      .catch(error => this.setState({log:error}));
+  }
+
+//update a deployment from fields (test purpose)
   updateDeployment =()=>{
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -180,7 +231,7 @@ class Deployment extends React.Component {
     fetch(`http://127.0.0.1:8000/apis/apps/v1/namespaces/default/deployments/${this.state.select}`, requestOptions)
       .then(response => response.text())
       .then(result => {
-        alert(JSON.parse(result).kind === "Deployment" ? "Success" : "Failure");
+        alert(JSON.parse(result).kind === "Deployment" ? "Successfully Updated" : "Failure");
         this.setState({log:result});
       })
       .catch(error => this.setState({log:error}));
@@ -200,7 +251,6 @@ class Deployment extends React.Component {
         </thead>
       <tbody>
       {this.state.list.map(item=>{
-        //console.log(item)
         return (
           <tr key={item.metadata.name}>
             <td>{item.metadata.name}</td>
@@ -270,7 +320,7 @@ class Deployment extends React.Component {
           })}
         </select>
         <br/><br/>
-        <button className="ui button" onClick={this.getDeploymentList}>List</button> 
+        <button className="ui button" onClick={this.getDeploymentList}>Reset</button> 
         {this.renderList()}
         <h3>Response log:</h3>
         <pre>
